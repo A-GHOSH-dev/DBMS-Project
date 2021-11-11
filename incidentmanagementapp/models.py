c59bb5a7170ba92f6fc58ff619e5d712726c37b0
Ananya Ghosh <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-11-12 01:52:06 +0530

    Add files via upload

--- incidentmanagementapp/models.py
+++ incidentmanagementapp/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+import os
+#from twilio.rest import Client
 
 # Create your models here.
+#signup
+class SignUp(models.Model):
+    emp_id = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)  
+    name = models.CharField(max_length=50)
+    Department = models.CharField(max_length=50)
+    FactoryNumber = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    password1 = models.CharField(max_length=20)
+    password2 = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.emp_id + ' ' + self.email + ' ' + self.name
+
+
+#login
+class Login(models.Model):
+    loginemp_id = models.CharField(max_length=50)
+    loginpassword = models.CharField(max_length=15)  
+   
+    
+    def __str__(self):
+        return self.loginemp_id + ' ' + self.loginpassword
+
+#adduser
+class AddUser(models.Model):
+    addemp_id = models.CharField(max_length=50)
+    addemail=models.CharField(max_length=50)
+    addname=models.CharField(max_length=50)
+    addDepartment=models.CharField(max_length=50)
+    addsection=models.CharField(max_length=50)
+    addrole=models.CharField(max_length=50)
+    addtraining=models.CharField(max_length=50)
+    addphone=models.CharField(max_length=50)
+    addpassword1=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.addemp_id + ' ' + self.addemail + ' ' + self.addname
+
+
+#IncidentReporting
+class IncidentReporting(models.Model):
+    datereport=models.CharField(max_length=50)
+    timereport=models.CharField(max_length=50)
+    dateincident=models.CharField(max_length=50)
+    reportedby=models.CharField(max_length=50)
+    incident_id=models.CharField(max_length=50)
+    timeincident=models.CharField(max_length=50)
+    locationincident=models.CharField(max_length=50)
+    incidentdesc=models.CharField(max_length=50)
+    incidentaction=models.CharField(max_length=50)
+    victimname=models.CharField(max_length=50)
+    victimrole=models.CharField(max_length=50)
+    victimemp_id=models.CharField(max_length=50)
+    victimcon_id=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.datereport + ' ' + self.dateincident
+
+#Assignleadinvestigator
+class AssignInvestigator(models.Model):
+    incident_id=models.CharField(max_length=50)
+    nameassignedinvestigator=models.CharField(max_length=50)
+    emailassignedinvestigator=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.incident_id + ' ' + self.emailassignedinvestigator
+
+
--- incidentmanagementapp/views.py
+++ incidentmanagementapp/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-
-#from incientmanagementsoftware.settings import RAZORPAY_API_KEY
 from django.shortcuts import render, HttpResponse, redirect
-#from KrishiSahayak.models import SignUp, Login, FoodOrder, ShopUpload, FarmerUpload, CustomerOrder
-#import razorpay
+#from incidentmanagementapp.models import SignUp, Login, AddUser, IncidentReporting, AssignInvestigator, CustomerOrder
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -12,21 +8,10 @@ from django.contrib import messages
 def index(request):
     #return HttpResponse("This is my home page")
     return render(request, 'index.html')
-def adduser(request):
-    #return HttpResponse("This is my home page")
-    return render(request, 'adduser.html')
-def incidentreport(request):
-    #return HttpResponse("This is my home page")
-    return render(request, 'incidentreport.html')
-def assignleadinvestigator(request):
-    #return HttpResponse("This is my home page")
-    return render(request, 'assignleadinvestigator.html')
 def allincidentslist(request):
     #return HttpResponse("This is my home page")
     return render(request, 'allincidentslist.html')
-def whywhyanalysis(request):
-    #return HttpResponse("This is my home page")
-    return render(request, 'whywhyanalysis.html')
+
 def specialanalysis(request):
     #return HttpResponse("This is my home page")
     return render(request, 'specialanalysis.html')
@@ -52,22 +37,17 @@ def handleSignup(request):
         return render(request, 'signup.html')
     if request.method=='POST':
         #Get parameters posted
-        username=request.POST['username']
+        emp_id=request.POST['emp_id']
         email=request.POST['email']
-        Sfirstname=request.POST['Sfirstname']
-        Slastname=request.POST['Slastname']        
+        name=request.POST['name']  
         Department=request.POST['Department']
         FactoryNumber=request.POST['FactoryNumber']
-        Scity=request.POST['Scity']
-        Saddress=request.POST['Saddress']
-        Spin=request.POST['Spin']
-        Sphone=request.POST['Sphone']
-        pass1=request.POST['pass1']
-        pass2=request.POST['pass1']
+        phone=request.POST['phone']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
 
-        myuser = User.objects.create_user(username, email, pass1)
-        myuser.first_name=Sfirstname
-        myuser.Last_name=Slastname
+        myuser = User.objects.create_user(emp_id, email, password1)
+        myuser.person_name=name
         myuser.save()
         messages.success(request, "Your account has been successfully created")
 
@@ -80,14 +60,14 @@ def handleLogin(request):
         return render(request, 'login.html')
         
     if request.method=='POST':
-        loginusername=request.POST['loginusername'] 
+        loginemp_id=request.POST['loginemp_id'] 
         loginpassword=request.POST['loginpassword']
 
-        user=authenticate(username=loginusername, password=loginpassword)
+        user=authenticate(emp_id=loginemp_id, password1=loginpassword)
         if user is not None:
             login(request, user)
             messages.success(request, "Successfully logged in")
-            return redirect('home')
+            return redirect('index')
         else:
             messages.error(request, "Invalid Credentials")
             return redirect('handleLogin')
@@ -98,3 +78,111 @@ def handleLogout(request):
     logout(request)
     messages.success(request, "Successfully Logged Out")
     return redirect('/')
+
+
+
+def adduser(request):
+    #return HttpResponse("This is my home page")
+    #return render(request, 'adduser.html')
+    if request.method=="POST":
+        addemp_id=request.POST['addemp_id']
+        addemail=request.POST['addemail']
+        addname=request.POST['addname']
+        addDepartment=request.POST['addDepartment']
+        addsection=request.POST['addsection']
+        addrole=request.POST['addrole']
+        addtraining=request.POST['addemp_id']
+        addphone=request.POST['addphone']
+        addpassword1=request.POST['addpassword1']
+
+        adduserdata = AddUser(addemp_id=addemp_id, addemail=addemail, addname=addname, addDepartment=addDepartment, addsection=addsection, addrole=addrole, addtraining=addtraining, addphone=addphone, addpassword1=addpassword1)  
+
+        adduserdata.save()
+        #return redirect('payment')
+        #return render(request,"foodsordernow.html",{"Orders":farmerbuyorder})
+        
+        
+    #return render(request,"foodsordernow.html",{"Orders":adduserdata})
+    return render(request,"adduser.html")
+
+
+
+
+
+def incidentreport(request):
+    #return HttpResponse("This is my home page")
+    #return render(request, 'incidentreport.html')
+    if request.method=="POST":
+        datereport=request.POST['datereport']
+        timereport=request.POST['timereport']
+        reportedby=request.POST['reportedby']
+        dateincident=request.POST['dateincident']
+        timeincident=request.POST['timeincident']
+        locationincident=request.POST['locationincident']
+        incidentdesc=request.POST['incidentdesc']
+        incidentaction=request.POST['incidentaction']
+        victimname=request.POST['victimname']
+        victimrole=request.POST['victimrole']
+        victimemp_id=request.POST['victimemp_id']
+        victimcon_id=request.POST['victimcon_id']
+
+        incidentreportingdata = IncidentReporting(datereport=datereport, timereport=timereport, reportedby=reportedby, dateincident=dateincident, timeincident=timeincident, locationincident=locationincident, incidentdesc=incidentdesc, incidentaction=incidentaction, victimname=victimname, victimrole=victimrole, victimemp_id=victimemp_id, victimcon_id=victimcon_id)  
+
+        incidentreportingdata.save()
+        #return redirect('payment')
+        #return render(request,"foodsordernow.html",{"Orders":farmerbuyorder})
+        
+        
+    #return render(request,"foodsordernow.html",{"Orders":adduserdata})
+    return render(request,"incidentreport.html")
+
+
+
+def assignleadinvestigator(request):
+    #return HttpResponse("This is my home page")
+    #return render(request, 'assignleadinvestigator.html')
+    if request.method=="POST":
+        incident_id=request.POST['incident_id']
+        nameassignedinvestigator=request.POST['nameassignedinvestigator']
+        emailassignedinvestigator=request.POST['emailassignedinvestigator']
+
+        assignleadinvestigatorreport = AssignInvestigator(incident_id=incident_id, nameassignedinvestigator=nameassignedinvestigator, emailassignedinvestigator=emailassignedinvestigator)  
+
+        assignleadinvestigatorreport.save()
+        #return redirect('payment')
+        #return render(request,"foodsordernow.html",{"Orders":farmerbuyorder})
+        
+        
+    #return render(request,"foodsordernow.html",{"Orders":adduserdata})
+    return render(request,"assignleadinvestigator.html")
+
+
+
+
+def whywhyanalysis(request):
+    #return HttpResponse("This is my home page")
+    #return render(request, 'whywhyanalysis.html')
+    if request.method=="POST":
+        datereport=request.POST['datereport']
+        timereport=request.POST['timereport']
+        reportedby=request.POST['reportedby']
+        dateincident=request.POST['dateincident']
+        timeincident=request.POST['timeincident']
+        locationincident=request.POST['locationincident']
+        incidentdesc=request.POST['incidentdesc']
+        incidentaction=request.POST['incidentaction']
+        victimname=request.POST['victimname']
+        victimrole=request.POST['victimrole']
+        victimemp_id=request.POST['victimemp_id']
+        victimcon_id=request.POST['victimcon_id']
+
+        incidentreportingdata = IncidentReporting(datereport=datereport, timereport=timereport, reportedby=reportedby, dateincident=dateincident, timeincident=timeincident, locationincident=locationincident, incidentdesc=incidentdesc, incidentaction=incidentaction, victimname=victimname, victimrole=victimrole, victimemp_id=victimemp_id, victimcon_id=victimcon_id)  
+
+        incidentreportingdata.save()
+        #return redirect('payment')
+        #return render(request,"foodsordernow.html",{"Orders":farmerbuyorder})
+        
+        
+    #return render(request,"foodsordernow.html",{"Orders":adduserdata})
+    return render(request,"whywhyanalysis.html")
+
